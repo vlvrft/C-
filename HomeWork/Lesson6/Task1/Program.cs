2059e4f9a522320be1d1e0403cba37914db9be9d
vlvrft <rufickk@gmail.com>
2023-03-14 00:15:10 +0400

    Add Home work Lesson 6

---
+++ HomeWork/Lesson6/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int[] InputArray(string text)
+{
+    System.Console.WriteLine(text);
+    return Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+}
+
+
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int Count(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+
+int[] myArray = InputArray("Введите число ");
+PrintArray(myArray);
+
+int result = Count(myArray);
+System.Console.WriteLine($"Количество чисел больше нуля в массиве равно {result}");
+
---
+++ HomeWork/Lesson6/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// // Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double InputNumber(string text)
+{
+    System.Console.Write(text);
+    return Math.Abs(Convert.ToDouble(Console.ReadLine()));
+}
+
+
+double IntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    double x = (-b2 + b1) / (-k1 + k2);
+    double y = k2 * x + b2;
+    return x;
+    return y;
+}
+
+void PrintNumber(double x, double y)
+{
+    Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+}
+
+
+double b1 = InputNumber("введите значение b1 >");
+double k1 = InputNumber("введите число k1 >");
+double b2 = InputNumber("введите значение b2 >");
+double k2 = InputNumber("введите число k2 >");
+PrintNumber(IntersectionPoint(b1, k1, b2, k2), IntersectionPoint(b1, k1, b2, k2));
